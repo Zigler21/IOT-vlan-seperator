27cb1f53696246d0e97e23c52a214eba4425ac2a
Zigler21 <joelwoot7@gmail.com>
2023-12-12 21:38:09 -0800

    added honeypot file

--- ids.py
+++ ids.py
@@ -1,6 +1,8 @@
 import scapy.all as scapy
 import pandas as pd
 import threading
+from urllib.parse import urlparse
+import requests
 
 class AnomalyIDS:
     def __init__(self, interface):
@@ -17,7 +19,22 @@ class AnomalyIDS:
     def process_packet(self, packet):
         # Simplified analysis: For example, count packets per second and compare against a threshold
         # In a real-world scenario, you'd have a more complex analysis here
+        if self.dectect_iot_attack(packet):
+            self.redirect_to_honeypot(packet)
         pass
+    def detect_iot_attack(self, packet):
+        # Logic to determine if the packet is targeting an IoT device
+        return True  # Placeholder
+
+    def redirect_to_honeypot(self, packet):
+        honeypot_url = "http://localhost:5001"
+        # This is a simplified and notional representation. Actual redirection would be more complex.
+        try:
+            if packet.haslayer(scapy.HTTP):
+                url = honeypot_url + urlparse(packet[scapy.HTTP].Path.decode()).path
+                requests.get(url)
+        except Exception as e:
+            print(f"Error redirecting to honeypot: {e}")
 
     def detect_anomaly(self, traffic_data):
         # Placeholder for anomaly detection logic
@@ -27,3 +44,4 @@ class AnomalyIDS:
     def log_alert(self, message):
         self.alerts.append(message)
         # todo: add to a log file on a server
+    
